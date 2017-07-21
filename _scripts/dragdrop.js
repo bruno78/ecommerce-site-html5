0f37aed409119ccc121c94a23615bdddd19b6d6d
bruno78 <devbruno78@gmail.com>
2017-07-21 03:21:21 -0400

    Change to old javascript to fix bug

--- _scripts/dragdrop.js
+++ _scripts/dragdrop.js
@@ -1,35 +1,35 @@
 $('.painting')
-  .bind('dragstart', (evt) => {
+  .bind('dragstart', function(evt) {
     evt.dataTransfer.setData('text', this.id);
     $('h2').fadeIn('fast');
   })
   .hover(
-    () => { $('div', this).fadeIn(); },
-    () => { $('div', this).fadeOut(); }
+    function() { $('div', this).fadeIn(); },
+    function() { $('div', this).fadeOut(); }
   );
 
-  $('#favorites')
-    .bind('dragover', (evt) => {
-      $('#favorites').css('background-color', 'gray');
-      evt.preventDefault();
-    })
-    .bind('dragleave', (evt) => {
-      $('#favorites').css('background-color', '#A858A3');
-      evt.preventDefault();
-    })
-    .bind('dragenter', (evt) => {
-      evt.preventDefault();
-    })
-    .bind('drop', (evt) => {
-      let id = evt.dataTransfer.getData('text'),
-      item = $('#' + id),
-      favList = $("#faves"),
-      prevFavItem = null;
-      prefFavItem = $('<li />', {
-        text : $('p:first', item).text(),
-        data : { id : id }
-      }).appendTo(favList);
-      $('#favorites').css('background-color', '#A858A3');
-      evt.stopPropagation();
-      return false;
-    });
+$('#favorites')
+  .bind('dragover', function(evt) {
+    $('#favorites').css('background-color', 'gray');
+    evt.preventDefault();
+  })
+  .bind('dragleave', function(evt) {
+    $('#favorites').css('background-color', '#A858A3');
+    evt.preventDefault();
+  })
+  .bind('dragenter', function(evt) {
+    evt.preventDefault();
+  })
+  .bind('drop', function(evt) {
+    let id = evt.dataTransfer.getData('text'),
+    item = $('#' + id),
+    favList = $("#faves"),
+    prevFavItem = null
+    prefFavItem = $('<li />', {
+      text : $('p:first', item).text(),
+      data : { id : id }
+    }).appendTo(favList);
+    $('#favorites').css('background-color', '#A858A3');
+    evt.stopPropagation();
+    return false;
+  })
